e82dbc455c1ccd3ba9b94a323292bd26f003efb4
Tampa, Moffitt Cancer Center, Shaw Immuno Systems Biology Lab <89986836+shawlab-moffitt@users.noreply.github.com>
2021-10-19 12:59:39 -0400

    Can loop through meta groupings

--- GettingStartedScript/GSEA_Sig_Table_Gen.R
+++ GettingStartedScript/GSEA_Sig_Table_Gen.R
@@ -18,25 +18,31 @@ library(readr)
 ##User Input
 
 #Assign expression data
-expr_file <- "~/R/PauloRodriguez/read_counts_normalized_geneSymbol_max_1FPKM.tsv"
+expr_file <- "~/R/USP7appsNstuff/data/HAUSP_htseq_fpkm_geneName_max.txt"
 
 #Assign meta data
-meta_file <- "~/R/PauloRodriguez/metafile.txt"
+meta_file <- "~/R/USP7appsNstuff/data/MS_meta.tsv"
 #Is there a header?
-header <- FALSE
+header <- TRUE
 
+#assign outfile path - where you want the enriched signatures table
+OutPath <- "~/R/USP7appsNstuff/data/"
+
+#Use prepared MSigDB gene set, if FALSE mouse model gene set used
+human <- FALSE
+
+
+##--OR--##
+
+
+##User designated gene set (OPTIONAL)
 #Assign user .gmt file if provided
 GeneSet_file.u.gmt <- "~/input/gmt/file/here.gmt"
-
+#or
 #Assign user gene set file if provided
 GeneSet_file.u.gs <- "~/input/geneset/file/here.tsv"
 #does this file have a header?
 header.gs <- TRUE
-
-#OR
-
-#Use prepared MSigDB gene set, if FALSE mouse model gene set used
-human <- FALSE
 
 
 
@@ -53,37 +59,52 @@ human <- FALSE
 ####----Reading Files----####
 
 ##reading expression data
-expr <- read.delim(expr_file, sep = '\t', header = T, row.names = 1, strip.white = T)
+expr <- read.delim(expr_file, sep = '\t', header = T, strip.white = T)
+colnames(expr)[1] <- "Gene"
+expr <- expr %>%
+  drop_na(Gene)
+row.names(expr) <- expr[,1]
+expr <- expr[,-1]
+colnames(expr) <- gsub("[_.-]", "_", colnames(expr))
 A <- as.matrix(expr)
 
 ##reading meta data
 #turn header on or off if in file
 meta <- read.delim(meta_file, sep = '\t', header = header, strip.white = T)
+colnames(meta) <- c("SampleName","Group")
+meta[,1] <- gsub("[_.-]", "_", meta[,1])
 
-##reading GeneSet data=
-#Generate gmt with user gmt file if provided
-if (file.exists(GeneSet_file.u.gmt)){
-  gmt <- read.gmt(GeneSet_file.u.gmt)
-}
-if (file.exists(GeneSet_file.u.gs)){
-  gmt <- read.delim(GeneSet_file.u.gs, header = header.gs, sep = '\t')
-  colnames(gmt) <- c("Term","Gene")
-}
+
+
+##reading GeneSet data
 
 #generate gmt with pre-loaded MSigDB gmt file - Human or Mouse
-if (file.exists(GeneSet_file.u) == FALSE){
+if (file.exists(GeneSet_file.u.gmt) == FALSE && file.exists(GeneSet_file.u.gs) == FALSE){
   if (human == TRUE) {
     GeneSet_file.m <- '~/R/USP7appsNstuff/data/MSigDB/msigdb_gsNsym_HS.tsv'
     gmt <- read.delim(GeneSet_file.m, header = T, sep = '\t')
   }
-  if (human == FALSE) {
-    GeneSet_file.m <- '~/R/USP7appsNstuff/data/MSigDB/msigdb_gsNsym_MS.tsv'
+  else if (human == FALSE) {
+    GeneSet_file.m <- '~/R/USP7appsNstuff/data/MSigDB/msigdb_gsNsym_MM.tsv'
     gmt <- read.delim(GeneSet_file.m, header = T, sep = '\t')
   }
 }
 
 
-##Create groups based off meta
+#Generate gmt with user gmt file if provided
+if (file.exists(GeneSet_file.u.gmt) == TRUE){
+  gmt <- read.gmt(GeneSet_file.u.gmt)
+}
+if (file.exists(GeneSet_file.u.gs) == TRUE){
+  gmt <- read.delim(GeneSet_file.u.gs, header = header.gs, sep = '\t')
+  colnames(gmt) <- c("Term","Gene")
+}
+
+
+
+
+
+####----Create groups based on meta----####
 levlst <- list()
 for (i in 1:length(levels(factor(meta[,2])))){
   group.i <- meta[,1][meta[,2] == levels(factor(meta[,2]))[i]]
@@ -100,66 +121,66 @@ list2env(levlst,globalenv())
 
 
 
-####----Signal-to-Noise Calculation----####
+####----Generate Enrich Sig Tables----####
+##loop through combinations of the groups
+##This will likely take sveral minutes due to the GSEA function
 
-#assign group from environment
-groupA.r <- groupA
-groupB.r <- groupB
-#assign outfile name and path - no extension
-OutPathAndName <- "..."
-
-
-A <- A + 0.00000001
-P = as.matrix(as.numeric(colnames(A) %in% groupA.r))
-n1 <- sum(P[,1])
-M1 <- A %*% P
-M1 <- M1/n1
-A2 <- A*A
-S1 <- A2 %*% P
-S1 <- S1/n1 - M1*M1 
-S1 <- sqrt(abs((n1/(n1-1)) * S1))
-P = as.matrix(as.numeric(colnames(A) %in% groupB.r))
-n2 <- sum(P[,1])
-M2 <- A %*% P
-M2 <- M2/n2
-A2 <- A*A
-S2 <- A2 %*% P
-S2 <- S2/n2 - M2*M2
-S2 <- sqrt(abs((n2/(n2-1)) * S2))
-rm(A2)
-# small sigma "fix" as used in GeneCluster
-S2 <- ifelse(0.2*abs(M2) < S2, S2, 0.2*abs(M2))
-S2 <- ifelse(S2 == 0, 0.2, S2)
-S1 <- ifelse(0.2*abs(M1) < S1, S1, 0.2*abs(M1))
-S1 <- ifelse(S1 == 0, 0.2, S1)
-M1 <- M1 - M2
-rm(M2)
-S1 <- S1 + S2
-rm(S2)
-s2n.matrix <- M1/S1
-
-
-####----Reformatting----####
-
-test1 <- as.data.frame(s2n.matrix)
-test1$GeneID <- rownames(test1)
-rownames(test1) <- NULL
-data <- dplyr::select(test1, GeneID, V1)
-data.gsea <- data$V1
-names(data.gsea) <- as.character(data$GeneID)
-s2n.matrix.s <- sort(data.gsea, decreasing = T)
-
-
-####----GSEA----####
-
-#perform GSEA
-gsea.res <- GSEA(s2n.matrix.s, TERM2GENE = gmt, verbose = F, pvalueCutoff = 1)
-#extract results and convert to tibble
-gsea.df <- as_tibble(gsea.res@result)
-
-#Write table to file
-write_delim(gsea.df, paste(OutPathAndName,".tsv",sep = ""), delim = "\t")
-
-
-
+g_combos <- combn(names(levlst), 2)
+for (i in 1:ncol(g_combos)){
+  ####----Create groups----####
+  #for calculations
+  group1 <- unlist(levlst[which(names(levlst) == g_combos[1,i])])
+  group2 <- unlist(levlst[which(names(levlst) == g_combos[2,i])])
+  #for file names
+  group1.n <- meta[,2][which(meta[,1] == group1[1])]
+  group2.n <- meta[,2][which(meta[,1] == group2[1])]
+  
+  ####----Signal-to-Noise Calculation----####
+  A <- A + 0.00000001
+  P = as.matrix(as.numeric(colnames(A) %in% group1))
+  n1 <- sum(P[,1])
+  M1 <- A %*% P
+  M1 <- M1/n1
+  A2 <- A*A
+  S1 <- A2 %*% P
+  S1 <- S1/n1 - M1*M1 
+  S1 <- sqrt(abs((n1/(n1-1)) * S1))
+  P = as.matrix(as.numeric(colnames(A) %in% group2))
+  n2 <- sum(P[,1])
+  M2 <- A %*% P
+  M2 <- M2/n2
+  A2 <- A*A
+  S2 <- A2 %*% P
+  S2 <- S2/n2 - M2*M2
+  S2 <- sqrt(abs((n2/(n2-1)) * S2))
+  rm(A2)
+  # small sigma "fix" as used in GeneCluster
+  S2 <- ifelse(0.2*abs(M2) < S2, S2, 0.2*abs(M2))
+  S2 <- ifelse(S2 == 0, 0.2, S2)
+  S1 <- ifelse(0.2*abs(M1) < S1, S1, 0.2*abs(M1))
+  S1 <- ifelse(S1 == 0, 0.2, S1)
+  M1 <- M1 - M2
+  rm(M2)
+  S1 <- S1 + S2
+  rm(S2)
+  s2n.matrix <- M1/S1
+  
+  ####----Reformatting----####
+  test1 <- as.data.frame(s2n.matrix)
+  test1$GeneID <- rownames(test1)
+  rownames(test1) <- NULL
+  data <- dplyr::select(test1, GeneID, V1)
+  data.gsea <- data$V1
+  names(data.gsea) <- as.character(data$GeneID)
+  s2n.matrix.s <- sort(data.gsea, decreasing = T)
+  
+  ####----GSEA----####
+  #perform GSEA
+  gsea.res <- GSEA(s2n.matrix.s, TERM2GENE = gmt, verbose = F, pvalueCutoff = 1)
+  #extract results and convert to tibble
+  gsea.df <- as_tibble(gsea.res@result)
+  
+  ####----Write to file----####
+  write_delim(gsea.df, paste(OutPath,"Enrich_Sig_Table_",group1.n,"v",group2.n,".tsv",sep = ""), delim = "\t")
+}
 
